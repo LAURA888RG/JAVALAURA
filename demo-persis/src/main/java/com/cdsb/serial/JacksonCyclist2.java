c0de431090b58f7000b001c6749c580bea6322b2
laura888rg <rodrilaura220@gmail.com>
2025-07-18 10:43:13 +0200

    today

--- demo-persis/src/main/java/com/cdsb/serial/JacksonCyclist2.java
+++ demo-persis/src/main/java/com/cdsb/serial/JacksonCyclist2.java
@@ -1,41 +1,39 @@
 package com.cdsb.serial;
 
-
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-public class JacksonCyclist2 extends JsonTools {
+public class JacksonCyclist2 {
 
     Cyclist cyclist;
     String pathCyclistJson = "demos-persis/resources/cyclist.json";
-    String pathBikesJson = "demos-persis/resources/bikes.json";
-
+    String pathBikesJson =  "demos-persis/resources/bikes.json";
+    //JsonTools jsonTools;
+    
     public JacksonCyclist2() {
+        //this.jsonTools = jsonTools;
         cyclist = new Cyclist("Pepe", LocalDate.of(2000, 10, 23));
         addBikes();
         System.out.println(cyclist);
     }
-
-    private void addBikes() {
+    private void addBikes(){
         Bicycle[] bikes;
-        bikes = this.fromJsonFile(pathBikesJson, Bicycle[].class);
+        bikes = Jsons.fromJsonFile(pathBikesJson,Bicycle[].class);
         for (int i = 0; i < bikes.length; i++) {
             cyclist.addBike(bikes[i]);
         }
     };
-
-    public void saveCyclist() {
+    
+    private void saveCyclist() {
         System.out.println("Saving cyclist...");
-        this.toJsonFile(cyclist, pathCyclistJson);
-        Cyclist savedCyclist = this.fromJsonFile(pathCyclistJson, Cyclist.class);
+        Jsons.toJsonFile(cyclist, pathCyclistJson);
+        Cyclist savedCyclist = Jsons.fromJsonFile(pathCyclistJson, Cyclist.class);
         System.out.println("Saved Cyclist: " + savedCyclist);
     }
+        
 
     public static void main(String[] args) {
-        JacksonCyclist2 jCyclist = new JacksonCyclist2();
-        jCyclist.saveCyclist();
+            JacksonCyclist2 jCyclist = new JacksonCyclist2();
+            jCyclist.saveCyclist();
     }
-
 }
+
