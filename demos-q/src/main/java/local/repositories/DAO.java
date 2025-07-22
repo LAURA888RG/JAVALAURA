d0d81be57fb87dfa41d7ba865adea15847b8c352
laura888rg <rodrilaura220@gmail.com>
2025-07-22 13:51:29 +0200

    today

---
+++ demos-q/src/main/java/local/repositories/DAO.java
@@ -0,0 +1,19 @@
+package local.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<E> {
+    List<E> findAll();
+
+    // getAll
+    <ID> Optional<E> findById(ID id);
+
+    E save(E entity);
+
+    void update(E entity);
+
+    void delete(E entity);
+    // <ID> void deleteById(ID id) ;
+
+}
